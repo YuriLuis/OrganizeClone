87121c8ae9d0f60efecad93e25d546716f6af0d7
@yuri_luis_garciia_pereira <yuri.pereira@hbsis.com.br>
2020-04-13 16:57:05 -0300

    arrumando detalhes do aplicativo

--- app/src/main/java/com/yuriluisgarciapereira/organizze/helper/ValidaCamposApp.java
+++ app/src/main/java/com/yuriluisgarciapereira/organizze/helper/ValidaCamposApp.java
@@ -1,7 +1,5 @@
 package com.yuriluisgarciapereira.organizze.helper;
 
-import com.yuriluisgarciapereira.organizze.exceptions.CampoVazioException;
-
 public class ValidaCamposApp {
 
     public static  boolean campoEhVazio(String campo){
